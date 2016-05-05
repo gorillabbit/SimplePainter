6aa1cfddfc1196c14a5dfcc1db47c52fc29c86a7
MODsyoukai <modsyoukaizenryoku@gmail.com>
2016-05-05 20:55:42 +0900

    JButtonをクリック→g.fillOval()に代入→点の大きさが変わる ってやりたいのにできないでこざる。

--- SimpleButton/src/simplebutton/SimpleButton.java
+++ SimpleButton/src/simplebutton/SimpleButton.java
@@ -18,62 +18,68 @@ import javax.swing.JPanel;
 
 public class SimpleButton extends JFrame implements ActionListener{
 
+	public int A;
+	public int B;
+
 	public static void main(String[] args) {
 		//フレームとキャンバス
 		SimpleButton frame = new SimpleButton("簡単なお絵かきソフト");
 		frame.setVisible(true);
-		
+
 		Canvas c = new Canvas();
 		frame.add(c);
 	}
-	
+
 	SimpleButton(String title){
 		setTitle(title);
 		setBounds(100,100,500,450);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		
+
 		//こっからボタン
 		JPanel p = new JPanel();
-		
+
 		JButton button =new JButton("PenSize");
 		int height = button.getMaximumSize().height;
 		button.setForeground(Color.RED);
 		button.setBackground(Color.LIGHT_GRAY);
 		button.setMaximumSize(new Dimension(200, height));
 		button.addActionListener(this);
-		
+
 		p.add(button);
 		p.setLayout(new BoxLayout(p,BoxLayout.PAGE_AXIS));
-		
+
 		Container contentPane = getContentPane();
 		contentPane.add(p, BorderLayout.CENTER);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-       int a = 20;
-       int b = 20;
+		A = 20;
+		B = 20;
 	}
 }
 //クリックで円を、ドラッグで線を書くように
 class Canvas extends JPanel implements MouseListener,MouseMotionListener {
 	private int x = -100;
 	private int y = -100;
-	 
+
     public Canvas() {
     	addMouseListener(this);
     	addMouseMotionListener(this);
         setSize(300, 100);
     }
- 
+
     @Override
     public void paintComponent(Graphics g) {
         g.setColor(Color.blue);
-        int a = 10;
-        int b = 10;
-		g.fillOval(x-a/2, y-b/2, a, b);
-    }    
+
+        SimpleButton a = new SimpleButton();
+        SimpleButton b = new SimpleButton();
+        a.A = 10;
+        b.B = 10;
+		g.fillOval(x-a.A/2, y-b.B/2, a.A, b.B);
+    }
         public void mouseClicked(MouseEvent e){
         }
         public void mousePressed(MouseEvent e){
